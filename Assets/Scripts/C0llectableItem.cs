6b01c7d1c2480aa24fa803f05e1cfaf84ba65e42
PyroSanta <matveyabramov09@gmail.com>
2024-03-30 23:41:00 +0300

    пвритд

---
+++ Assets/Scripts/C0llectableItem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class C0llectableItem : MonoBehaviour
+{
+    public UnityEvent action;
+    public bool destroyAfterCollected = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var controller = other.GetComponent<CharacterController>();
+        if (!controller) return;
+
+        action.Invoke();
+
+        if (!destroyAfterCollected) return;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timeStart = 60;
+    public Text timerText;
+    public float endTime = 0;
+    public GameObject GameWining;
+    public GameObject gameplayUI;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       timerText.text =  timeStart.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeStart -= Time.deltaTime;
+        timerText.text = Mathf.Round(timeStart).ToString();
+        if(timeStart <= 0)
+        {
+            gameplayUI.SetActive(false);
+            GameWining.SetActive(true);
+
+
+            GetComponent<PlayerController>().enabled = false;
+            GetComponent<FireballCaster>().enabled = false;
+            GetComponent<CameraRotation>().enabled = false;
+        }
+
+        
+    }
+}
